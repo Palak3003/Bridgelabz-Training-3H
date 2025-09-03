5260eface3cdede449c63b63e41c3fb6ae1000b2
Palak3003 <bhardwajpalak3003@gmail.com>
2025-09-03 17:09:23 +0530

    [Palak] add vehicle

---
+++ Constructor/instanceAndClass/Vehicle.java
@@ -0,0 +1,21 @@
+package Constructor.instanceAndClass;
+public class Vehicle {
+    private String ownerName;
+    private String vehicleType;
+    private static double registrationFee;
+
+    public Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    public void displayVehicleDetails() {
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Fee: ₹" + registrationFee);
+    }
+
+    public static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+}
